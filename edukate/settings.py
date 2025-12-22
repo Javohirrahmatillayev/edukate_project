69a1a208e998aa3c58c2207c2fede9be4679fbe8
Javohir <deozey7@gmail.com>
2025-12-22 17:21:51 +0500

    twitter and github login added

--- edukate/settings.py
+++ edukate/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.twitter',
     'app',
 ]
 
